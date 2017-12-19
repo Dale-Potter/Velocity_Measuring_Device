f65bd5329fec449e88916022c593696f01385f70
Dale Potter <dalepotter103@gmail.com>
2017-12-19 16:57:58 +0000

    Script for measuring distance with ultasonic sensor, includes filter to smooth data. Next step is to start a timer when distance increases and stop timer when change in distance stops. Then use this time and change in distance to calculate average speed.

---
+++ Distance_with_filter/Distance_with_filter.ino
@@ -0,0 +1,38 @@
+#include <microsmooth.h>
+
+// defines pin numbers
+const int trigPin = 9;
+const int echoPin = 10;
+
+// defines variables
+long duration;
+int distance;
+int pulse = 25;
+uint16_t *ptr;
+
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+  
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(pulse);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+
+// Prints the distance on the Serial Monitor/Plotter
+Serial.println(ema_filter(distance, ptr));
+}
